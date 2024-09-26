dcbf7fc6bcfd7a2fd904c3567611b325e0b93db1
Léo Stewart <leokun@me.com>
2024-09-26 14:31:33 +0200

    add users module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
+import { UsersModule } from './users/users.module';
 
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [UsersModule],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
